639d8ca357397e7d5016634ff257fe1f0e2c32f6
oksanatkach <oksanatkachua@gmail.com>
2020-10-03 13:16:01 +0300

    dat2

--- NER/__init__.py
+++ NER/__init__.py
@@ -3,4 +3,7 @@ from spacy.pipeline import EntityRuler
 from .matcher_util import add_gaz_to_patterns
 
 nlp = spacy.load('en_core_web_sm')
-#@todo: replace the original NER model in the nlp pipeline with the ruler
+ruler = EntityRuler(nlp)
+ruler.add_patterns(add_gaz_to_patterns())
+nlp.add_pipe(ruler)
+nlp.remove_pipe('ner')
